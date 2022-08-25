f317bb3bd807b725cb334b5c24ebcc92540d3236
João Olívio <80077983+JoaoOlivio@users.noreply.github.com>
2022-08-25 17:23:15 -0300

    1º Versão

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>LOGIN - Wake </title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Seja Bem Vindo!</h1>
+                                    </div>
+                                    <?php
+                                    if (isset($_SESSION['nao_autenticado'])) :
+                                    ?>
+                                        <div class="notification is-danger">
+                                            <p style="color:red;">ERRO: Usuário ou senha inválidos.</p>
+                                        </div>
+                                    <?php
+                                    endif;
+                                    unset($_SESSION['nao_autenticado']);
+                                    ?>
+                                    <form class="user" action="loginValidacao.php" method="POST">
+                                        <div class="form-group">
+                                            <input type="text" name="usuario" class="form-control form-control-user" aria-describedby="emailHelp" placeholder="Login">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="senha" class="form-control form-control-user" placeholder="Senha">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+
+</body>
+
+</html>
